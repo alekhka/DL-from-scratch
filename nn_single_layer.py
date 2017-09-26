d907d67ce8d2dba3abe60631fbdde8114d60d0b8
Alekh Karkada Ashok <alekhka@gmail.com>
2017-09-26 17:16:30 +0530

    Made changes to single layer NN

--- nn_single_layer.py
+++ nn_single_layer.py
@@ -7,7 +7,7 @@ Created on Sat Sep 23 11:35:56 2017
 """
 
 import numpy as np
-import matplotlib.pyplot as mplt
+import matplotlib.pyplot as mplt #Used to plot error over iterations remove if not needed
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
@@ -15,6 +15,7 @@ def sigmoid(x):
 def sigderiv(x):
     y = 1/(1+np.exp(-x))
     return y*(1-y)
+
 n_iters=10000
 np.random.seed(1)
 X = np.array([  [0,0,1],
@@ -28,18 +29,18 @@ wt3 = 2*np.random.random((3,1)) - 1
 
 for i in xrange(n_iters):
     l0 = X
-    #l1 = np.dot(l0,wt0) #Regression
-    l1 = sigmoid(np.dot(l0,wt3)) #Classification
+
+    #l1 = np.dot(l0,wt0) #Uncomment this for Regression
+    l1 = sigmoid(np.dot(l0,wt3)) #Uncomment this for Classification
     l1_err = Y - l1
     
-    #l1_grad = l1_err * 0.001 #Regression
-    l1_grad = l1_err * sigderiv(np.dot(l0,wt3)) #Classification
+    #l1_grad = l1_err * 0.001 #Regression error 
+    l1_grad = l1_err * sigderiv(np.dot(l0,wt3)) #Classification error
     
     wt3 = wt3 + np.dot(l0.T,l1_grad)
     
     if i%1==0 and i<5000:
         err.append(np.sum(np.abs(l1_err)/4))
-        #print '{0:.10f}'.format(np.sum(l1_err)/4)
 
 print "Op after training"
 np.set_printoptions(suppress=True)
