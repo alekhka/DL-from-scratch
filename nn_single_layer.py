97d3d57eccd5a003f81146992c3cab4573c26fad
Alekh Karkada Ashok <alekhka@gmail.com>
2017-09-26 17:13:49 +0530

    Added Single Layer neural network

---
+++ nn_single_layer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 11:35:56 2017
+
+@author: alekh
+"""
+
+import numpy as np
+import matplotlib.pyplot as mplt
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def sigderiv(x):
+    y = 1/(1+np.exp(-x))
+    return y*(1-y)
+n_iters=10000
+np.random.seed(1)
+X = np.array([  [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1]
+        ])
+Y = np.array([[0,0,1,1]]).T
+err=[]
+wt3 = 2*np.random.random((3,1)) - 1
+
+for i in xrange(n_iters):
+    l0 = X
+    #l1 = np.dot(l0,wt0) #Regression
+    l1 = sigmoid(np.dot(l0,wt3)) #Classification
+    l1_err = Y - l1
+    
+    #l1_grad = l1_err * 0.001 #Regression
+    l1_grad = l1_err * sigderiv(np.dot(l0,wt3)) #Classification
+    
+    wt3 = wt3 + np.dot(l0.T,l1_grad)
+    
+    if i%1==0 and i<5000:
+        err.append(np.sum(np.abs(l1_err)/4))
+        #print '{0:.10f}'.format(np.sum(l1_err)/4)
+
+print "Op after training"
+np.set_printoptions(suppress=True)
+print l1
+err = np.asarray(err)
+mplt.plot(err)
+mplt.show()
+print wt3
